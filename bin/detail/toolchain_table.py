9b410ec3fd368828e63b1aa5550cb7ab47aa304f
mix <248587660@qq.com>
2018-05-16 11:58:15 +0800

    ffff

--- bin/detail/toolchain_table.py
+++ bin/detail/toolchain_table.py
@@ -164,6 +164,7 @@ if platform.system() == 'Linux':
 
 if platform.system() == 'Darwin':
   toolchain_table += [
+      Toolchain('ios-11-3', 'Xcode', ios_version='11.3'),
       Toolchain('ios-11-2', 'Xcode', ios_version='11.2'),
       Toolchain('ios-11-1', 'Xcode', ios_version='11.1'),
       Toolchain('ios-11-0', 'Xcode', ios_version='11.0'),
